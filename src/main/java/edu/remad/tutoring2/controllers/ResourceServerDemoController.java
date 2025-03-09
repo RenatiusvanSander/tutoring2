474dcd52b0c9fe69bcf253f1ff9646b62d6cb936
Remy Meier <remad@web.de>
2025-03-09 20:31:47 +0100

    feature/oauth-resource-server: role was set

--- src/main/java/edu/remad/tutoring2/controllers/ResourceServerDemoController.java
+++ src/main/java/edu/remad/tutoring2/controllers/ResourceServerDemoController.java
@@ -1,16 +1,19 @@
 package edu.remad.tutoring2.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ResourceServerDemoController {
 	
+	@PreAuthorize("hasRole('client_user')")
 	@GetMapping("/v1/demo")
 	public String answerDemo() {
 		return "Spring 6 and Keycloak works";
 	}
 	
+	@PreAuthorize("hasRole('client_admin')")
 	@GetMapping("/v1/demoAdmin")
 	public String answerDemoAdmin() {
 		return "Spring 6 and Keycloaks Admin";
